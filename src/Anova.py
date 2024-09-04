6244a60e14aa460624876b8e80b7f79779ae4fa0
Bella Chan. <chanyanan.sethpitak2@lseg.com>
2024-09-04 19:05:24 +0700

    Adjust Anova, ZScore to support demonstration

--- src/Anova.py
+++ src/Anova.py
@@ -1,11 +1,14 @@
 import numpy as np
 from scipy.stats import f_oneway
+import pandas as pd
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 group1 = [23, 25, 27, 22, 30, 31, 29, 24, 28, 26]
 group2 = [35, 32, 37, 34, 36, 33, 38, 35, 39, 31]
-group3 = [48, 45, 47, 44, 46, 49, 48, 47, 43, 45]
+group3 = [48, 47, 46, 45, 47, 48, 47, 46, 45, 47]  
 group4 = [56, 52, 54, 55, 58, 59, 60, 57, 53, 51]
-group5 = [63, 65, 67, 66, 68, 62, 64, 69, 61, 66]
+group5 = [49, 50, 48, 47, 49, 50, 49, 48, 50, 49] 
+
 
 f_statistic, p_value = f_oneway(group1, group2, group3, group4, group5)
 
@@ -17,3 +20,12 @@ if p_value < alpha:
     print("Reject the null hypothesis: At least one group mean is significantly different.")
 else:
     print("Fail to reject the null hypothesis: No significant difference between group means.")
+
+
+data = np.concatenate([group1, group2, group3, group4, group5])
+
+groups = ['Group 1'] * len(group1) + ['Group 2'] * len(group2) + ['Group 3'] * len(group3) + ['Group 4'] * len(group4) + ['Group 5'] * len(group5)
+
+tukey_result = pairwise_tukeyhsd(data, groups, alpha=0.05)
+
+print(tukey_result)
--- src/ZScore.py
+++ src/ZScore.py
@@ -1,14 +1,12 @@
 import numpy as np
 
-data = [23, 25, 27, 22, 30, 31, 29, 24, 28, 26]
+data = [23, 25, 27, 6,22, 30, 31, 29, 24, 28, 26, 8, 99]
 
 mean = np.mean(data)
 std_dev = np.std(data)
 
 z_scores = [(x - mean) / std_dev for x in data]
 
-print(f"Mean: {mean}")
-print(f"Standard Deviation: {std_dev}")
 print(f"Z-Scores: {z_scores}")
 
 for i, z in enumerate(z_scores):
@@ -18,4 +16,6 @@ for i, z in enumerate(z_scores):
         direction = "below"
     else:
         direction = "at"
-    print(f"Data point {data[i]} is {z:.2f} standard deviations {direction} the mean.")
+
+    outlier_status = "Outlier" if abs(z) > 3 else "Not an Outlier"
+    print(f" {data[i]} is {z:.2f} -{outlier_status}")
