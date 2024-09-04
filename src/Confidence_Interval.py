e3c04cb04de7bbebbc1c9b7737f433e2cb7638e5
Bella Chan. <chanyanan.sethpitak2@lseg.com>
2024-09-04 19:30:17 +0700

    interprettion friendly

--- src/Confidence_Interval.py
+++ src/Confidence_Interval.py
@@ -6,10 +6,10 @@ data = [12, 15, 14, 10, 13, 18, 20, 15, 14, 13]
 mean = np.mean(data)
 std_dev = np.std(data, ddof=1)  # ddof=1 provides the sample standard deviation
 n = len(data)
-confidence_level = 0.95
+confidence_level = 0.99
 
 t_critical = stats.t.ppf((1 + confidence_level) / 2, df=n-1)
 margin_of_error = t_critical * (std_dev / np.sqrt(n))
 confidence_interval = (float(mean - margin_of_error), float(mean + margin_of_error))
 
-print(f"From Sampling - Under {confidence_level:.2f} confidence level: Range to fall in Population: {confidence_interval}")
+print(f"From Sampling - Under {100*confidence_level:.2f}% confidence level: Range to fall in Population: {confidence_interval}")
