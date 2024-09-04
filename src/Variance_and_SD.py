cf28b80cf95965b20c5c510471aecfce38f7c326
Bella Chan. <chanyanan.sethpitak2@lseg.com>
2024-09-04 19:13:08 +0700

    adjust Variance_and_SD.py for more demo friendly.

--- src/Variance_and_SD.py
+++ src/Variance_and_SD.py
@@ -1,13 +1,21 @@
 import numpy as np
 
-data = [10, 12, 14, 14, 18, 20, 22, 25, 30, 35, 100, -20]
+#data = [10, 12, 14, 14, 18, 20, 22, 25, 30, 35, 100, -20]
+#data = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+data = [11, 12, 13, 9, 8, 10, 14, 20, 10, 17]
 
-variance = np.var(data, ddof=0)  # Population variance
-# Use ddof=1 for sample variance instead of population variance
+mean = np.mean(data)
+variance = np.var(data, ddof=0)  
+std_dev = np.std(data, ddof=0)  
 
-std_dev = np.std(data, ddof=0)  # Population standard deviation
-# Use ddof=1 for sample standard deviation instead of population standard deviation
-
-# Print the results
 print(f"Variance: {variance}")
 print(f"Standard Deviation: {std_dev}")
+
+if std_dev < 10: #important condition
+    interpretation = "Close around the mean."
+elif std_dev < 20:
+    interpretation = "Moderate Spread around the mean."
+else:
+    interpretation = "Significant Spread around the mean."
+
+print(f"Interpretation: {interpretation}")
